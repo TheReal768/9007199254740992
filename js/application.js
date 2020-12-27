1d1ac3895505bfa4fc7153605774ec5bd99719ac
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-12-26 19:50:39 -0600

    Add AI

--- js/application.js
+++ js/application.js
@@ -1,18 +1,70 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+var game;
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-
-  // TODO: This code is in need of a refactor (along with the rest)
-  var storage     = new LocalStorageManager;
-  var noticeClose = document.querySelector(".notice-close-button");
-  var notice      = document.querySelector(".app-notice");
-  if (storage.getNoticeClosed()) {
-    notice.parentNode.removeChild(notice);
-  } else {
-    noticeClose.addEventListener("click", function () {
-      notice.parentNode.removeChild(notice);
-      storage.setNoticeClosed(true);
-      ga("send", "event", "notice", "closed");
-    });
-  }
+  game = new GameManager(8, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
+
+var last = '';
+var dir = 0;
+var cnt = 0;
+
+var mover = undefined;
+
+function doMovementPattern(moveType) {
+  mover = setInterval(moveType, 20);
+}
+
+function stopMovement() {
+  mover = clearInterval(mover);
+}
+
+function auto() {
+  if (game == null || typeof(game) === "undefined") {
+    return;
+  }
+  var item = document.getElementById('tile-container');
+  if (item.innerHTML == last) {
+    if (++cnt > 1) {
+      dir = 1 - dir;
+      cnt = 0;
+    }
+  }
+  last = item.innerHTML;
+  if (0 === dir) {
+    game.move(0);
+    game.move(3);
+  } else {
+    game.move(0);
+    game.move(1);
+  }
+}
+
+function swing() {
+  if (game == null || typeof(game) === "undefined") {
+    return;
+  }
+  var item = document.getElementById('tile-container');
+  if (item.innerHTML == last) {
+    if (++cnt > 1) {
+      dir = 1 - dir;
+      cnt = 0;
+    }
+  }
+  last = item.innerHTML;
+  if (0 === dir) {
+    game.move(0);
+    game.move(2);
+  } else {
+    game.move(1);
+    game.move(3);
+  }
+}
+
+function swirl() {
+  dir = (dir + 1) % 4;
+  game.move(dir);
+}
+
+function random() {
+  game.move(Math.floor(Math.random() * 4));
+}
